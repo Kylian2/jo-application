3fa21ad2413d4071cefcc71274d62458a918c821
Esteban <162978612+EstebanDebug@users.noreply.github.com>
2024-05-30 17:58:37 +0200

    feat : finalisation Accueil

--- sae1256/src/vues/vueAccueil.java
+++ sae1256/src/vues/vueAccueil.java
@@ -10,61 +10,92 @@ public class vueAccueil extends JPanel{
 	    protected JLabel ImageLabel1, ImageLabel2, ImageLabel3, ImageLabel4;
 	
 	    public vueAccueil() {
-	    	// Création des Panels
+	    	 // Création des panels
 	        JPanel PanelText = new JPanel();
 	        JPanel PanelIMG = new JPanel();
-	        JPanel PanelIMG1 = new JPanel();
-	        JPanel PanelIMG2 = new JPanel();
-	        JPanel PanelIMG3 = new JPanel();
-	        JPanel PanelIMG4 = new JPanel();
-	        JLabel TexteAccueil = new JLabel("Salut Louis !");
-	        
-	        TexteAccueil.setFont(new Font("Arial", Font.PLAIN, 36));
+	        JLayeredPane PanelIMG1 = new JLayeredPane();
+	        JLayeredPane PanelIMG2 = new JLayeredPane();
+	        JLayeredPane PanelIMG3 = new JLayeredPane();
+	        JLayeredPane PanelIMG4 = new JLayeredPane();
 
-	        // Exportation des images
+	        // Créations d'autres éléments pour mettre dans les panels
+	        JButton BoutonIMG1 = new JButton("Voir les Résultats 	");
+	        BoutonIMG1.setBackground(Color.RED);
+	        BoutonIMG1.setForeground(Color.WHITE);
+
+	        JButton BoutonIMG2 = new JButton("Accéder aux équipes 	");
+	        BoutonIMG2.setBackground(Color.BLUE);
+	        BoutonIMG2.setForeground(Color.WHITE);
+
+	        JButton BoutonIMG3 = new JButton("Voir le planning		");
+	        BoutonIMG3.setBackground(Color.GREEN);
+	        BoutonIMG3.setForeground(Color.WHITE);
+
+	        JButton BoutonIMG4 = new JButton("Voir les athlètes 	");
+	        BoutonIMG4.setBackground(Color.YELLOW);
+	        BoutonIMG4.setForeground(Color.WHITE);
+
+	        JLabel TexteAccueil = new JLabel("Salut Louis !");
+	        TexteAccueil.setFont(new Font("Arial", Font.PLAIN, 32));
+
+	        // On importe les images depuis le dossier img
 	        ImageIcon image1 = new ImageIcon("img/img_accueil1.png");
 	        ImageIcon image2 = new ImageIcon("img/img_accueil2.png");
 	        ImageIcon image3 = new ImageIcon("img/img_accueil3.png");
 	        ImageIcon image4 = new ImageIcon("img/img_accueil4.png");
 
-	        // On met les images dans des Label pour simplifier leur utilisation 
+	        // On met les images dans des Labels
 	        JLabel ImageLabel1 = new JLabel(image1);
 	        JLabel ImageLabel2 = new JLabel(image2);
 	        JLabel ImageLabel3 = new JLabel(image3);
 	        JLabel ImageLabel4 = new JLabel(image4);
+	        
+	        // Configuration des panels avec JLayeredPane
+	        configurationDesLayeredPane(PanelIMG1, ImageLabel1, BoutonIMG1);
+	        configurationDesLayeredPane(PanelIMG2, ImageLabel2, BoutonIMG2);
+	        configurationDesLayeredPane(PanelIMG3, ImageLabel3, BoutonIMG3);
+	        configurationDesLayeredPane(PanelIMG4, ImageLabel4, BoutonIMG4);
 
-	        setLayout(new BorderLayout());
-
-	        // Ajout des marges autour des images
-	        ImageLabel1.setBorder(new EmptyBorder(10, 10, 10, 10));
-	        ImageLabel2.setBorder(new EmptyBorder(10, 10, 10, 10));
-	        ImageLabel3.setBorder(new EmptyBorder(10, 10, 10, 10));
-	        ImageLabel4.setBorder(new EmptyBorder(10, 10, 10, 10));
+	        // Les layout et border sur les panels de plus haut niveau
+	        PanelText.setLayout(new FlowLayout(FlowLayout.LEFT));
+	        PanelText.setBorder(new EmptyBorder(10, 0, 20, 0));
+	        PanelIMG.setBorder(new EmptyBorder(0, 0, 0, 200));
+	        PanelIMG.setLayout(new GridLayout(2, 2, 15, 15));
 
 	        // On ajoute dans les panels de plus haut niveau
-	        PanelIMG.setLayout(new GridLayout(2, 2, 10, 10)); // Ajout d'espaces entre les cellules de GridLayout
-	        PanelText.setLayout(new FlowLayout(FlowLayout.LEFT));
-	        
-	        PanelIMG1.add(ImageLabel1);
-	        PanelIMG2.add(ImageLabel2);
-	        PanelIMG3.add(ImageLabel3);
-	        PanelIMG4.add(ImageLabel4);
-	        
 	        PanelIMG.add(PanelIMG1);
 	        PanelIMG.add(PanelIMG2);
 	        PanelIMG.add(PanelIMG3);
 	        PanelIMG.add(PanelIMG4);
 	        PanelText.add(TexteAccueil);
 
-	        // Ajout d'une marge autour du PanelIMG pour espacer du PanelText
-	        PanelIMG.setBorder(new EmptyBorder(20, 0, 0, 0)); // marge de 20 pixels en haut
-
-	        // On modifie le placement des Panel principaux
+	        // Répartit les panels dans panel de niveau 2 dans le panel principal
+	        setLayout(new BorderLayout());
 	        add(PanelText, BorderLayout.NORTH);
 	        add(PanelIMG, BorderLayout.CENTER);
-	}	    
+	    }
 
-    // Main method for testing the panel
+	    // Permet le superpositionnement des éléments entre images et boutons
+	    private void configurationDesLayeredPane(JLayeredPane layer, JLabel imageLabel, JButton button) {
+	    	
+	    	// Récupération de la taille des éléments
+	        int LargeurImage = imageLabel.getIcon().getIconWidth();
+	        int Longueurimage = imageLabel.getIcon().getIconHeight();
+	        int Largeurbouton = 200;
+	        int Longueurbouton = 30;
+
+	        layer.setPreferredSize(new Dimension(LargeurImage, Longueurimage));
+
+	        // Positionnement du bouton en bas à droite
+	        imageLabel.setBounds(0, 0, LargeurImage, Longueurimage);
+	        button.setBounds(LargeurImage - Largeurbouton - 10, Longueurimage - Longueurbouton - 10, Largeurbouton, Longueurbouton); 
+
+	        // Intégration dans le layerPane
+	        layer.add(imageLabel, JLayeredPane.DEFAULT_LAYER);
+	        layer.add(button, JLayeredPane.PALETTE_LAYER);
+	    }	    
+
+    // Permet de créer la fenêtre de l'interface graphique
     public static void main(String[] args) {
         JFrame fenetre = new JFrame("Accueil");
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
