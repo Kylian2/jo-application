1e97d1dca06724abc1da001cea356791a1de6287
Kylian2 <kylian.richard@gmail.com>
2024-06-06 22:29:50 +0200

    feat: controleurSession
    Le controleur est presque terminé.
    Mise en place de :
     - methode d'ajout
     - validation des paramètres
     - fonctionnement JTable
     - Lien Discipline -> Epreuve
     - Lien Discpline -> Athlete
     - Navigation entre les pages Apercu, Planning et Ajouter

--- sae1256/src/vues/VueAjouterSession.java
+++ sae1256/src/vues/VueAjouterSession.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -12,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -21,17 +25,22 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JComponent;
 
 import controleurs.ControleurAthlete;
+import controleurs.ControleurSession;
 import modeles.ApplicationJo;
+import modeles.Athlete;
+import modeles.Discipline;
+import modeles.Epreuve;
 
 /**
  * Vue permettant d'ajouter une session
  * @author klentz
+ * @author kylianrichard
  */
 
 public class VueAjouterSession extends JPanel {
 		
 	
-	VueAjouterSession(ControleurAthlete controleur){
+	VueAjouterSession(ControleurSession controleur){
 		this.setLayout(new BorderLayout());
 		
         // initialisation des panel globaux
@@ -61,28 +70,41 @@ public class VueAjouterSession extends JPanel {
         JComboBox<String> disciplineTexte = new JComboBox<String>();
         disciplineTexte.addItem("Choisir une discipline");
         disciplinePanel.setBackground(Color.WHITE);
-        
-        
         disciplinePanel.add(discipline);
         disciplinePanel.add(disciplineTexte);
+        
+        for(Discipline disciplineFromApp : controleur.application.disciplinesList) {
+        	disciplineTexte.addItem(disciplineFromApp.getNom());
+        }
         
         JPanel epreuvesPanel = new JPanel();
         epreuvesPanel.setLayout(new GridLayout(1,2));
         JLabel epreuves = new JLabel("Epreuves :     ", SwingConstants.RIGHT);
-        JComboBox<String> epreuvesTexte = new JComboBox<String>();
-        epreuvesTexte.addItem("Choisir une épreuve");
+        JComboBox<String> epreuveTexte = new JComboBox<String>();
+        epreuveTexte.addItem("Choisir une épreuve");
         epreuvesPanel.setBackground(Color.WHITE);
         epreuvesPanel.add(epreuves);
-        epreuvesPanel.add(epreuvesTexte);
+        epreuvesPanel.add(epreuveTexte);
         
-        JPanel typePanel = new JPanel();
-        typePanel.setLayout(new GridLayout(1,2));
-        JLabel type = new JLabel("Type :     ", SwingConstants.RIGHT);
-        JComboBox<String> typeTexte = new JComboBox<String>();
-        typeTexte.addItem("Choisir un type d'épreuve");
-        typePanel.setBackground(Color.WHITE);
-        typePanel.add(type);
-        typePanel.add(typeTexte);
+        //Modifie la liste des epreuves en fonction de la discipline selectionnée
+        disciplineTexte.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Obtient l'élément sélectionné
+                String selectedItem = (String) disciplineTexte.getSelectedItem();
+                if(!selectedItem.equalsIgnoreCase("Choisir une discipline")) {
+                	ArrayList<Epreuve> epreuves = controleur.getEpreuveDiscipline(selectedItem);
+                	epreuveTexte.removeAllItems();
+                	for(Epreuve epreuveFromControleur : epreuves) {
+                		epreuveTexte.addItem(epreuveFromControleur.getNom());
+                	}
+                }else {
+                	epreuveTexte.removeAllItems();
+                	epreuveTexte.addItem("Choisir une épreuve");
+                }
+            }
+
+        });
         
         JPanel datePanel = new JPanel();
         datePanel.setLayout(new GridLayout(1,2));
@@ -95,7 +117,7 @@ public class VueAjouterSession extends JPanel {
         JPanel debutPanel = new JPanel();
         debutPanel.setLayout(new GridLayout(1,2));
         JLabel debut = new JLabel("Début :     ", SwingConstants.RIGHT);
-        JTextField debutTexte = new JTextField("Entrez l'heure de début");
+        JTextField debutTexte = new JTextField("Heure de début (hh:mm)");
         debutPanel.setBackground(Color.WHITE);
         debutPanel.add(debut);
         debutPanel.add(debutTexte);
@@ -108,20 +130,18 @@ public class VueAjouterSession extends JPanel {
         dureePanel.add(duree);
         dureePanel.add(dureeTexte);
         
-        JPanel categoriePanel = new JPanel();
-        categoriePanel.setLayout(new GridLayout(1,2));
-        JLabel categorie = new JLabel("Catégorie :     ", SwingConstants.RIGHT);
-        JComboBox<String> categorieTexte = new JComboBox<String>();
-        categorieTexte.addItem("Choisir une catégorie");
-        categoriePanel.setBackground(Color.WHITE);
-        categoriePanel.add(categorie);
-        categoriePanel.add(categorieTexte);
+        JPanel lieuPanel = new JPanel();
+        lieuPanel.setLayout(new GridLayout(1,2));
+        JLabel lieu = new JLabel("Lieu :     ", SwingConstants.RIGHT);
+        JTextField lieuTexte = new JTextField("Entrez un lieu");
+        lieuPanel.setBackground(Color.WHITE);
+        lieuPanel.add(lieu);
+        lieuPanel.add(lieuTexte);
         
         JPanel sexePanel = new JPanel();
         sexePanel.setLayout(new GridLayout(1,2));
         JLabel sexe = new JLabel("Sexe :     ", SwingConstants.RIGHT);
         JComboBox<String> sexeTexte = new JComboBox<String>();
-        sexeTexte.addItem("Choisir un sexe");
         sexeTexte.addItem("Homme");
         sexeTexte.addItem("Femme");
         sexePanel.setBackground(Color.WHITE);
@@ -134,8 +154,6 @@ public class VueAjouterSession extends JPanel {
         info.add(disciplinePanel);
         info.add(Box.createHorizontalStrut(10));
         info.add(epreuvesPanel);
-        info.add(Box.createHorizontalStrut(10));			// créer un espace entre chaque panel
-        info.add(typePanel);
         info.add(Box.createHorizontalStrut(10));
         info.add(datePanel);
         info.add(Box.createHorizontalStrut(10));
@@ -143,7 +161,7 @@ public class VueAjouterSession extends JPanel {
         info.add(Box.createHorizontalStrut(10));
         info.add(dureePanel);
         info.add(Box.createHorizontalStrut(10));
-        info.add(categoriePanel);
+        info.add(lieuPanel);
         info.add(Box.createHorizontalStrut(10));
         info.add(sexePanel);
         
@@ -186,27 +204,56 @@ public class VueAjouterSession extends JPanel {
         DefaultTableModel modele = new DefaultTableModel();
         JTable tableau = new JTable(modele);
         
+        ArrayList<Athlete> athletesDisponibles = new ArrayList<Athlete>();
+        ArrayList<Athlete> athletesAjoutes = new ArrayList<Athlete>();
+        
+        modele.addColumn("CodePays");
         modele.addColumn("NomAthlete");
         modele.addColumn("PrenomAthlete");
-        modele.addColumn("CodePays");
         
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"7","8","9"});
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"4","5","6"});
-        modele.addRow(new Object[] {"7","8","9"});
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"4","5","6"});
-        modele.addRow(new Object[] {"7","8","9"});
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"4","5","6"});
-        modele.addRow(new Object[] {"7","8","9"});
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"4","5","6"});
-        modele.addRow(new Object[] {"7","8","9"});
-        modele.addRow(new Object[] {"1","2","3"});
-        modele.addRow(new Object[] {"4","5","6"});
-        modele.addRow(new Object[] {"7","8","9"});
+        disciplineTexte.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Obtient l'élément sélectionné
+                String discipline = (String) disciplineTexte.getSelectedItem();
+                if(!discipline.equalsIgnoreCase("Choisir une discipline")) {
+                	athletesDisponibles.removeAll(athletesDisponibles);
+                	athletesAjoutes.removeAll(athletesAjoutes);
+                	athleteTexte.removeAllItems();
+                	for(Athlete athlete : controleur.application.athletesList) {
+                		if(athlete.getDiscipline().getNom().equalsIgnoreCase(discipline)) {
+                			athleteTexte.addItem(athlete.getNom() + " " + athlete.getPrenom());
+                			athletesDisponibles.add(athlete);
+                		}
+                	}
+                }else {
+                	athletesDisponibles.removeAll(athletesDisponibles);
+                	athleteTexte.addItem("Choisir un athlète");
+                }
+            }
+
+        });
+        
+        ajouterAthlete.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	int athleteIndex = athleteTexte.getSelectedIndex();
+            	if(!((String) athleteTexte.getSelectedItem()).equalsIgnoreCase("Choisir un athlète")) {
+            		Athlete athlete = athletesDisponibles.get(athleteIndex);
+            		String codePays = athlete.getPays().getCode();
+        			String nomAthlete = athlete.getNom();
+        			String prenomAthlete = athlete.getPrenom();
+        			modele.addRow(new Object[] {codePays,nomAthlete,prenomAthlete});
+        			athletesAjoutes.add(athlete);
+        			athletesDisponibles.remove(athleteIndex);
+        			athleteTexte.removeAllItems();
+        			for(Athlete athleteDisponible : athletesDisponibles) {
+        				athleteTexte.addItem(athleteDisponible.getNom() + " " + athleteDisponible.getPrenom());
+        			}
+            	}
+            }
+
+        });
         
         // ajout des compostant au mainJpane
         JScrollPane panneau = new JScrollPane(tableau);
@@ -233,7 +280,35 @@ public class VueAjouterSession extends JPanel {
         boutons.setBackground(Color.WHITE);
         boutons.setLayout(new GridLayout(2,3));
         JButton valider = new JButton("Valider");
+        
+        valider.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//Recuperer les informations contenu dans les inputs
+				String discipline = (String) disciplineTexte.getSelectedItem();
+				String epreuve = (String) epreuveTexte.getSelectedItem();
+				String date = dateTexte.getText();
+				String debut = debutTexte.getText();
+				String sexe = (String) sexeTexte.getSelectedItem();
+				String duree = dureeTexte.getText();
+				String lieu = lieuTexte.getText();
+				ArrayList<Athlete> athleteParticipants = athletesAjoutes;
+				boolean creation = controleur.ajouterSession(date, debut, duree, lieu, sexe, discipline, epreuve, athleteParticipants);
+				
+			}
+        	
+        });
+        	
         JButton annuler = new JButton("Annuler");
+        
+        annuler.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controleur.retour();	
+			}
+        });
         
 		// changer la couleur de fond des boutons
         annuler.setBackground(Color.GRAY);
@@ -254,29 +329,5 @@ public class VueAjouterSession extends JPanel {
         // ajout des composant principaux
         add(info, BorderLayout.WEST);
         add(mainPanel, BorderLayout.CENTER);
-	}
-	
-	public static void main(String[] args) {
-		// Creer une fenetre
-		JFrame fenetre = new JFrame ();
-		fenetre.setSize(960,540);
-		fenetre.setLocationRelativeTo(null); 
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-
-		// Creer une instance de ma classe
-		ApplicationJo applicationJo = new ApplicationJo();
-		
-		applicationJo.recuperer();
-		
-		ControleurAthlete controleur = new ControleurAthlete(applicationJo);
-
-		VueAjouterSession p = new VueAjouterSession(controleur);
-
-		// Ajouter mon instance dans un des conteneurs de la fen?tre
-		fenetre.add(p);
-
-		// Afficher la fenetre
-		fenetre.setVisible(true);
 	}
 }
--- sae1256/src/vues/VueApercuSession.java
+++ sae1256/src/vues/VueApercuSession.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -13,12 +15,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import controleurs.ControleurEquipe;
 import controleurs.ControleurSession;
 import modeles.*;
 
 public class VueApercuSession extends JPanel {
-	
-	ApplicationJo application;
 	
 	Dimension dimension;
 	
@@ -151,10 +152,25 @@ public class VueApercuSession extends JPanel {
         buttonPanel.setBackground(Color.WHITE);
         buttonPanel.setMaximumSize(new Dimension((int)dimension.getWidth(), 210));
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-        JButton VoirToutButton = new JButton("Voir le planning complet");
-        VoirToutButton.setBackground(Color.RED);
-        VoirToutButton.setForeground(Color.WHITE);
-        buttonPanel.add(VoirToutButton, BorderLayout.EAST);
+        JButton voirToutButton = new JButton("Voir le planning complet");
+        voirToutButton.setBackground(Color.RED);
+        voirToutButton.setForeground(Color.WHITE);
+        
+        voirToutButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controleur.application.mainPanel.removeAll();
+				controleur.application.mainPanel.add(new VuePlanning(controleur));
+                // Rafraîchir le conteneur
+				controleur.application.mainPanel.revalidate();
+				controleur.application.mainPanel.repaint();
+				
+			}
+        	
+        });
+        
+        buttonPanel.add(voirToutButton, BorderLayout.EAST);
         
         add(header);
         add(sessionEnCoursPanel);
